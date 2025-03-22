ac8b524ea8ddb231354f89ea7212f6214fcef16c
monicajuderajkumar <monica.rajan05@gmail.com>
2025-03-22 14:09:00 -0500

    Main branch commit update

--- src/main/java/com/SalesForce/Base/BasePage.java
+++ src/main/java/com/SalesForce/Base/BasePage.java
@@ -23,6 +23,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 //Feature_code_2 - First Commit
+// Base Page - Main Commit
 public class BasePage {
 
 	WebDriver driver;
