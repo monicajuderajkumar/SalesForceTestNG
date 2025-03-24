780c657d94449ddd4a1195a6d7250449737fdc5a
monicajuderajkumar <monica.rajan05@gmail.com>
2025-03-24 12:28:34 -0500

    Adding new file BaseTheory

---
+++ src/main/java/com/SalesForce/Base/BaseTheory.java
@@ -0,0 +1,11 @@
+package com.SalesForce.Base;
+
+public class BaseTheory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Base Theory - Feature Code 2- Commit1");
+	}
+
+}
